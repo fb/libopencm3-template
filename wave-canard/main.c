dbc3cf9ac753b746d3bca9c9df566c7c21b6bf17
Fabian Bartschke <fabian@bartschke.de>
2020-03-18 13:38:11 +0100

    main: add extern void usleep() needed by canard.c

--- wave-canard/main.c
+++ wave-canard/main.c
@@ -1,4 +1,13 @@
 #include <stdint.h>
+#include <canard.h>
+
+void usleep(void);
+
+// called with usleep(1000) from canard.c
+extern void usleep(void)
+{
+    // TODO implement 1 ms delay
+}
 
 int main(void) {
 	/* add your own code */
