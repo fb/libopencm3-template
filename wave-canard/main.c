7b345a90be1debf982b81707b1ff815ece5405b8
Fabian Bartschke <fabian@bartschke.de>
2020-03-18 13:27:35 +0100

    add canard.c to build

--- wave-canard/main.c
+++ wave-canard/main.c
@@ -1,9 +1,7 @@
-#include "api.h"
-#include "api-asm.h"
+#include <stdint.h>
 
 int main(void) {
 	/* add your own code */
 	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+	return 0;
 }
